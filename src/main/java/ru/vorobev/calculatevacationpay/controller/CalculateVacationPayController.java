3fa976f75dca91aff618b20cf7f9a5e76b72d35a
maxim <vorobevkovrov@gmail.com>
2024-09-20 20:27:27 +0300

    Controller return response entity

--- src/main/java/ru/vorobev/calculatevacationpay/controller/CalculateVacationPayController.java
+++ src/main/java/ru/vorobev/calculatevacationpay/controller/CalculateVacationPayController.java
@@ -2,6 +2,8 @@ package ru.vorobev.calculatevacationpay.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,16 @@ import java.math.BigDecimal;
 @Slf4j
 @AllArgsConstructor
 @RestController("/api/v1")
+@Validated
 public class CalculateVacationPayController {
     private final CalculateVacationPay calculateVacationPay;
 
     @GetMapping("/calculate")
-    public BigDecimal calculateVacationPay(@RequestParam BigDecimal employeeIncomeForYear,
-                                           @RequestParam int numberOfVacationDays) {
+    public ResponseEntity<BigDecimal> calculateVacationPay(@RequestParam BigDecimal employeeIncomeForYear,
+                                               @RequestParam int numberOfVacationDays) {
         log.info("CalculateVacationPayController /calculate");
-        return calculateVacationPay.calculateVacationPay(employeeIncomeForYear, numberOfVacationDays);
+       // return calculateVacationPay.calculateVacationPay(employeeIncomeForYear, numberOfVacationDays);
+        return ResponseEntity.ok(calculateVacationPay.calculateVacationPay(employeeIncomeForYear, numberOfVacationDays));
     }
 }
 //TODO проверка входящих параметров
--- src/main/java/ru/vorobev/calculatevacationpay/service/CalculateVacationPay.java
+++ src/main/java/ru/vorobev/calculatevacationpay/service/CalculateVacationPay.java
@@ -1,7 +1,5 @@
 package ru.vorobev.calculatevacationpay.service;
 
-import jakarta.annotation.Nonnull;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,18 +8,16 @@ import org.springframework.validation.annotation.Validated;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+
 @Slf4j
 @Service
 @AllArgsConstructor
 @RequiredArgsConstructor
-@Validated
 public class CalculateVacationPay {
     private final static BigDecimal AVERAGE_NUMBER_OF_DAYS_IN_YEAR = new BigDecimal("29.3");
     private final static BigDecimal COUNT_MONTHS_IN_YEAR = new BigDecimal("12");
     private BigDecimal amountOfVacationPayPerDay;
     private BigDecimal vacationPayAmountForVacation;
-    private BigDecimal employeeIncomeForYear;
-    private int numberOfVacationDays;
 
     public BigDecimal calculateAmountOfVacationPerDay(BigDecimal employeeIncomeForYear) {
         log.info("calculateAmountOfVacationPerDay");
