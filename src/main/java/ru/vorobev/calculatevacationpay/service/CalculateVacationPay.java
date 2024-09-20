1362d9472e2dbf6bd91f555f2fc4abc399a98e02
Maxim Vorobev-Kovrov <vorobevkovrov@gmail.com>
2024-09-20 16:37:45 +0300

    Some code added

--- src/main/java/ru/vorobev/calculatevacationpay/service/CalculateVacationPay.java
+++ src/main/java/ru/vorobev/calculatevacationpay/service/CalculateVacationPay.java
@@ -1,9 +1,12 @@
 package ru.vorobev.calculatevacationpay.service;
 
+import jakarta.annotation.Nonnull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -11,8 +14,10 @@ import java.math.RoundingMode;
 @Service
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Validated
 public class CalculateVacationPay {
     private final static BigDecimal AVERAGE_NUMBER_OF_DAYS_IN_YEAR = new BigDecimal("29.3");
+    private final static BigDecimal COUNT_MONTHS_IN_YEAR = new BigDecimal("12");
     private BigDecimal amountOfVacationPayPerDay;
     private BigDecimal vacationPayAmountForVacation;
     private BigDecimal employeeIncomeForYear;
@@ -21,7 +26,7 @@ public class CalculateVacationPay {
     public BigDecimal calculateAmountOfVacationPerDay(BigDecimal employeeIncomeForYear) {
         log.info("calculateAmountOfVacationPerDay");
         return amountOfVacationPayPerDay = new BigDecimal(String.valueOf(employeeIncomeForYear))
-                .divide(BigDecimal.valueOf(12), 2, RoundingMode.HALF_UP)
+                .divide(COUNT_MONTHS_IN_YEAR, 2, RoundingMode.HALF_UP)
                 .divide(AVERAGE_NUMBER_OF_DAYS_IN_YEAR, 2, RoundingMode.HALF_UP);
     }
 
